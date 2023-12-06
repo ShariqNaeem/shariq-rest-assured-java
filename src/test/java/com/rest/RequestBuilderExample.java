c7291f331b99af5d28e202eb84a233ab2ac867b6
ShariqNaeem <muhammad.shariq64@gmail.com>
2023-12-06 21:37:44 +0500

    example of request builder

--- src/test/java/com/rest/RequestBuilderExample.java
+++ src/test/java/com/rest/RequestBuilderExample.java
@@ -1,7 +1,10 @@
 package com.rest;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.http.client.methods.RequestBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,80 +19,28 @@ public class RequestBuilderExample {
 
     @BeforeClass
     public void init(){
-        requestSpecification = with() // we can use given() method as well
-                .baseUri("https://reqres.in")
-                .queryParam("page=2")
-                .log().all(); // for logging te request
-    }
-    @Test
-    public void ExampleRequestSpecification() {
-        //given() return the request specification So we can send the request specification in the parameter..
-//        RequestSpecification requestSpecification = given()
-//                .baseUri("https://reqres.in")
-//                .queryParam("page=2");
+        //Without Method Chaining
+        RequestSpecBuilder requestBuilder= new RequestSpecBuilder();
+        requestBuilder.setBaseUri("https://reqres.in");
+        requestBuilder.addQueryParam("page=2");
+        requestBuilder.log(LogDetail.ALL);
 
-        //given(requestSpecification) Both will work
-        given().spec(requestSpecification)
-        .when()
-                .get("/api/users")
-        .then()
-                .log().all()
-                .assertThat()
-                .statusCode(200)
-                // "$" define the root-object of the response body
-                .body("$", hasKey("page"), // Validate the single-key
+        //With Method Chaining
+        RequestSpecBuilder requestBuilder1 = new RequestSpecBuilder();
+        requestBuilder1.setBaseUri("https://reqres.in").addQueryParam("page=2").log(LogDetail.ALL);
 
-                        // Validate the multiple keys
-                        "$", allOf(
-                                hasKey("per_page"),
-                                hasKey("total_pages"),
-                                hasKey("data")
-                        ),
-
-                        "data[3]", hasEntry("id", 4),
-
-                        "data", hasSize(6) // Another way to validate the size/length of array
-                );
+        //requestSpecification = requestBuilder.build();
+        requestSpecification = requestBuilder1.build();
     }
 
-    @Test
-    public void ExampleRequestSpecification2() {
-        //given() return the request specification So we can send the request specification in the parameter..
-//        RequestSpecification requestSpecification = given()
-//                .baseUri("https://reqres.in")
-//                .queryParam("page=2");
-
-        //given(requestSpecification) Both will work
-        given().spec(requestSpecification)
-                .when()
-                .get("/api/users")
-                .then()
-                .log().all()
-                .assertThat()
-                .statusCode(200)
-                // "$" define the root-object of the response body
-                .body("$", hasKey("page"), // Validate the single-key
-
-                        // Validate the multiple keys
-                        "$", allOf(
-                                hasKey("per_page"),
-                                hasKey("total_pages"),
-                                hasKey("data")
-                        ),
-                        "data[3]", hasEntry("id", 4)
-                );
-    }
-
-
-    // Here are the syntax to change from BDD to NON-BDD
     @Test
     public void statusCodeValidation() {
-        Response response = requestSpecification.get("/api/users").then().log().all().extract().response();
+        Response response = given(requestSpecification).get("/api/users").then().log().all().extract().response();
         assertThat(response.statusCode(), is(equalTo(200)));
     }
     @Test
     public void responseBodyValidation() {
-        Response response = requestSpecification.get("/api/users").then().log().all().extract().response();
+        Response response = given(requestSpecification).get("/api/users").then().log().all().extract().response();
         assertThat(response.path("data[0].email"), is(equalTo("george.bluth@reqres.in")));
     }
 }
