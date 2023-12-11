00eb8bbd39d9f1f35231bd186dbf647dc3a1cc99
ShariqNaeem <muhammad.shariq64@gmail.com>
2023-12-11 18:43:20 +0500

    added log to a file

--- src/test/java/com/rest/loggingFilter.java
+++ src/test/java/com/rest/loggingFilter.java
@@ -5,6 +5,10 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
@@ -18,6 +22,21 @@ public class loggingFilter {
                 .filter(new ResponseLoggingFilter(LogDetail.BODY))
                 .when()
                     .get("/get")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("jsonSchemaValidator.json"));
+    }
+
+    @Test
+    public void loggingToFileTest() throws FileNotFoundException {
+        PrintStream printStream = new PrintStream(new File("logToFile.log"));
+        given()
+                .baseUri("https://postman-echo.com")
+                .filter(new RequestLoggingFilter(printStream))
+                .filter(new ResponseLoggingFilter(printStream))
+                .when()
+                .get("/get")
                 .then()
                 .assertThat()
                 .statusCode(200)
